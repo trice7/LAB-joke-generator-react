71da4e9a09ee5bb0bc9a378b455624a46b5b4777
Thomas Rice <ricethomasj@gmail.com>
2023-07-11 20:22:33 -0500

    added basic styling

--- pages/joke.js
+++ pages/joke.js
@@ -1,19 +1,8 @@
 import React, { useState } from 'react';
 import getJoke from '../api/jokeData';
 
-// let theJoke = {};
-// const newJoke = async () => {
-//   theJoke = await getJoke;
-// };
-
 const Joke = () => {
-  // const theJoke = await getJoke;
-  // const newJoke = () => {
-  //   getJoke().then((item) => {
-  //     theJoke = item;
-  //   });
-  // };
-  const [text, setText] = useState('Welcome to the joke generator! Click the button below to get a joke.');
+  const [text, setText] = useState('Welcome to the joke generator!');
   const [btn, setBtn] = useState('Get Joke');
   const [deliv, setDeliv] = useState('');
   let theJoke = {};
@@ -38,7 +27,7 @@ const Joke = () => {
   return (
     <>
       <h2 className="setup">{ text }</h2>
-      {btn === 'Want Another?' ? (<h2 className="delivery">{deliv}</h2>) : null}
+      {btn === 'Want Another?' ? (<h5 className="delivery">{deliv}</h5>) : null}
       <button type="button" className="joke-btn" onClick={btn === 'Get Joke' || btn === 'Want Another?' ? newJoke : btnState}>{ btn }</button>
     </>
   );
